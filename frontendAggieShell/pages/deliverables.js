7b8611325f5aff09ade7fdaf06d0946cc482550e
Justin Xu <justinxu@Justins-MacBook-Air.local>
2021-11-29 20:18:19 -0800

    Updating landing page

---
+++ frontendAggieShell/pages/deliverables.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React from "react";
+import Link from "next/link";
+
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import Footer from "components/Footers/Footer.js";
+import Tabs from "components/Tabs.js";
+
+export default function Deliverables() {
+  return (
+    <>
+      <IndexNavbar fixed />
+    </>
+    );
+  }
